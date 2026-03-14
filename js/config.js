9e34e59179075d677d4246f83075893cb829e7a9
Luiz Fernando <fernandovieirasantos10@gmail.com>
2026-03-14 10:58:55 -0400

    tentando rodar no versel

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// ============================================================
+//  js/config.js — Configurações sensíveis
+// ============================================================
+
+export const firebaseConfig = {
+  apiKey:            "AIzaSyD8zs9a57gnNYI5tsACBzkE-Uk-_Bk1XJU",
+  authDomain:        "controle-de-pneus-79051.firebaseapp.com",
+  projectId:         "controle-de-pneus-79051",
+  storageBucket:     "controle-de-pneus-79051.firebasestorage.app",
+  messagingSenderId: "682588647980",
+  appId:             "1:682588647980:web:3e6d58897745dc086f5789",
+  measurementId:     "G-YC69HEYC1D",
+};
